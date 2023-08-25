e6c9987a94612b4be563c08eb1585d0201e30bfb
gabrielardevol <artsdevol@gmail.com>
2023-08-25 21:06:41 +0200

    -

---
+++ scripts/unfoldLayout.js
@@ -0,0 +1,7 @@
+const unfoldLayout = () => {
+    document.querySelector("#card-layout").style.width = "100%"
+}
+
+const foldLayout = () => {
+    document.querySelector("#card-layout").style.width = "0%"
+}
